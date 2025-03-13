cb175ebbd70065cee50214f3a5d12fdc01c85ad3
CatalinCocosila <92211735+CatalinCocosila@users.noreply.github.com>
2025-03-13 20:22:27 +0200

    incercare de reparare3

--- blockly.js
+++ blockly.js
@@ -1,78 +1,82 @@
 window.sendCommand = function(command) {
-    if (window.BlocBlocklyChannel) {
-        window.BlocBlocklyChannel.postMessage(command);
-        console.log("📡 [JS] Comandă trimisă către Flutter: " + command);
-        console.log(" Comandă trimisă către Flutter: " + command);
-    } else {
-        console.log("⚠️ [JS] WebView nu este integrat cu Flutter. Comandă: " + command);
-        console.log("WebView nu este integrat cu Flutter. Comandă: " + command);
+    try {
+        if (window.BlocBlocklyChannel) {
+            window.BlocBlocklyChannel.postMessage(command);
+        }
+    } catch (error) {
+        console.error("Eroare la trimiterea comenzii către Flutter:", error);
     }
 };
 
-
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("🔄 Inițializare Blockly...");
-
     Blockly.Blocks['move_forward'] = {
         init: function() {
             this.appendDummyInput()
-                .appendField(new Blockly.FieldImage("icons/arrow-up-solid.svg", 20, 20, "*")) // SVG local
+                .appendField(new Blockly.FieldImage("icons/arrow-up-solid.svg", 20, 20, "*"))
                 .appendField("Mergi înainte");
             this.setPreviousStatement(true);
             this.setNextStatement(true);
             this.setColour(230);
         }
     };
-    Blockly.JavaScript.forBlock['move_forward'] = function(block) {
+    Blockly.JavaScript['move_forward'] = function(block) {
         return 'window.sendCommand("UP");\n';
     };
 
     Blockly.Blocks['move_backward'] = {
         init: function() {
             this.appendDummyInput()
-                .appendField(new Blockly.FieldImage("icons/arrow-down-solid.svg", 20, 20, "*")) // SVG local
+                .appendField(new Blockly.FieldImage("icons/arrow-down-solid.svg", 20, 20, "*"))
                 .appendField("Mergi înapoi");
             this.setPreviousStatement(true);
             this.setNextStatement(true);
             this.setColour(230);
         }
     };
-    Blockly.JavaScript.forBlock['move_backward'] = function(block) {
+    Blockly.JavaScript['move_backward'] = function(block) {
         return 'window.sendCommand("DOWN");\n';
     };
 
     Blockly.Blocks['turn_left'] = {
         init: function() {
             this.appendDummyInput()
-                .appendField(new Blockly.FieldImage("icons/arrow-left-solid.svg", 20, 20, "*")) // SVG local
+                .appendField(new Blockly.FieldImage("icons/arrow-left-solid.svg", 20, 20, "*"))
                 .appendField("Rotire stânga");
             this.setPreviousStatement(true);
             this.setNextStatement(true);
             this.setColour(230);
         }
     };
-    Blockly.JavaScript.forBlock['turn_left'] = function(block) {
+    Blockly.JavaScript['turn_left'] = function(block) {
         return 'window.sendCommand("LEFT");\n';
     };
 
     Blockly.Blocks['turn_right'] = {
         init: function() {
             this.appendDummyInput()
-                .appendField(new Blockly.FieldImage("icons/arrow-right-solid.svg", 20, 20, "*")) // SVG local
+                .appendField(new Blockly.FieldImage("icons/arrow-right-solid.svg", 20, 20, "*"))
                 .appendField("Rotire dreapta");
             this.setPreviousStatement(true);
             this.setNextStatement(true);
             this.setColour(230);
         }
     };
-    Blockly.JavaScript.forBlock['turn_right'] = function(block) {
+    Blockly.JavaScript['turn_right'] = function(block) {
         return 'window.sendCommand("RIGHT");\n';
     };
 
-    const workspace = Blockly.inject('blocklyDiv', { toolbox: document.getElementById('toolbox') });
+    try {
+        const workspace = Blockly.inject('blocklyDiv', { toolbox: document.getElementById('toolbox') });
 
-    window.runProgram = function() {
-        const code = Blockly.JavaScript.workspaceToCode(workspace);
-        console.log("Cod generat:\n" + code);
-    };
+        window.runProgram = function() {
+            try {
+                const code = Blockly.JavaScript.workspaceToCode(workspace);
+                eval(code);
+            } catch (error) {
+                console.error( " Eroare la execuția codului Blockly:", error);
+            }
+        };
+    } catch (error) {
+        console.error("Eroare la inițializarea Blockly:", error);
+    }
 });
