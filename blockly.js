db9b05c483785b6b6926313c156e5daa3ed9b888
CatalinCocosila <92211735+CatalinCocosila@users.noreply.github.com>
2025-03-13 19:58:08 +0200

    modificare aspect blocuri

--- blockly.js
+++ blockly.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("🔄 Inițializare Blockly...");
-    
+
     Blockly.Blocks['move_forward'] = {
         init: function() {
-            this.appendDummyInput().appendField("🔼Mergi înainte");
+            this.appendDummyInput()
+                .appendField(new Blockly.FieldImage("icons/arrow-up-solid.svg", 20, 20, "*")) // SVG local
+                .appendField("Mergi înainte");
             this.setPreviousStatement(true);
             this.setNextStatement(true);
             this.setColour(230);
@@ -12,10 +14,12 @@ document.addEventListener("DOMContentLoaded", function() {
     Blockly.JavaScript.forBlock['move_forward'] = function(block) {
         return 'window.sendCommand("UP");\n';
     };
-    
+
     Blockly.Blocks['move_backward'] = {
         init: function() {
-            this.appendDummyInput().appendField("🔽Mergi înapoi");
+            this.appendDummyInput()
+                .appendField(new Blockly.FieldImage("icons/arrow-down-solid.svg", 20, 20, "*")) // SVG local
+                .appendField("Mergi înapoi");
             this.setPreviousStatement(true);
             this.setNextStatement(true);
             this.setColour(230);
@@ -24,10 +28,12 @@ document.addEventListener("DOMContentLoaded", function() {
     Blockly.JavaScript.forBlock['move_backward'] = function(block) {
         return 'window.sendCommand("DOWN");\n';
     };
-    
+
     Blockly.Blocks['turn_left'] = {
         init: function() {
-            this.appendDummyInput().appendField("⬅Rotire stânga");
+            this.appendDummyInput()
+                .appendField(new Blockly.FieldImage("icons/arrow-left-solid.svg", 20, 20, "*")) // SVG local
+                .appendField("Rotire stânga");
             this.setPreviousStatement(true);
             this.setNextStatement(true);
             this.setColour(230);
@@ -36,10 +42,12 @@ document.addEventListener("DOMContentLoaded", function() {
     Blockly.JavaScript.forBlock['turn_left'] = function(block) {
         return 'window.sendCommand("LEFT");\n';
     };
-    
+
     Blockly.Blocks['turn_right'] = {
         init: function() {
-            this.appendDummyInput().appendField("➡Rotire dreapta");
+            this.appendDummyInput()
+                .appendField(new Blockly.FieldImage("icons/arrow-right-solid.svg", 20, 20, "*")) // SVG local
+                .appendField("Rotire dreapta");
             this.setPreviousStatement(true);
             this.setNextStatement(true);
             this.setColour(230);
@@ -48,13 +56,11 @@ document.addEventListener("DOMContentLoaded", function() {
     Blockly.JavaScript.forBlock['turn_right'] = function(block) {
         return 'window.sendCommand("RIGHT");\n';
     };
-    
-    var workspace = Blockly.inject('blocklyDiv', { toolbox: document.getElementById('toolbox') });
-    console.log(" Blockly merge");
-    
+
+    const workspace = Blockly.inject('blocklyDiv', { toolbox: document.getElementById('toolbox') });
+
     window.runProgram = function() {
-        var code = Blockly.JavaScript.workspaceToCode(workspace);
-        console.log("Cod:\n" + code);
-       
+        const code = Blockly.JavaScript.workspaceToCode(workspace);
+        console.log("Cod generat:\n" + code);
     };
 });
